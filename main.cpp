ea6184effb34c8065d591524093e40a108c90787
PWSZ Nowy Sącz <cpp.programowanie@gmail.com>
2019-11-25 15:20:00 +0100

    OSTATNI

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "tstring.h"
+#include "operatory.h"
 using namespace std;
 
 auto fun(const char* s) {
@@ -12,10 +13,11 @@ int main() {
     TString s3 = s2;
     s1 = fun("drugi obiekt");  
     
+    cout << s1 + s2 << endl;
+    s1 = s1 + "KONIEC";
     cout << s1 << endl;
-    cin >> s1;
-    cout << s1 << endl;
-
+    s3 += s3;
+    cout << s3 << endl;
 
     cout << "----------------" << endl;
 }
